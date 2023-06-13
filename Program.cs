8e27ac95c1bcabbf689adfff3b5d00c8ab8aa3f6
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-13 11:08:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EqualRowColumnPairs
+{
+	internal class Program
+	{
+		public class EqualRowColumnPairs
+		{
+			public int EqualPairs(int[][] grid)
+			{
+				HashSet<string> set = new();
+				for (int r = 0; r < grid.Length; ++r)
+				{
+					set.Add(string.Join(", ", grid[r]));
+				}
+				int equalPairs = 0;
+				for (int c = 0; c < grid[0].Length; ++c)
+				{
+					string colStr = string.Join(", ", grid[c]);
+					if (set.Contains(colStr))
+					{
+						++equalPairs;
+					}
+				}
+				return equalPairs;
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
